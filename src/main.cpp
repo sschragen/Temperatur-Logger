bdc8fca6dd3747fdc128d74edef27bcfd2e5f0f1
sschragen <stephanus@schragen-net.de>
2019-10-18 23:52:11 +0200

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,72 @@
 #include <Arduino.h>
-#include <TaskScheduler.h>
+#include <Task.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-// Callback methods prototypes
-void t1Callback();
+const char* ssid = "Schragen2.4"; // Enter your WiFi name
+const char* password =  "warpdrive"; // Enter WiFi password
+const char* mqttServer = "prodesk.fritz.box";
+const int   mqttPort = 37181;
+const char* mqttUser = "otfxknod";
+const char* mqttPassword = "nSuUc1dDLygF";
 
-Task t1(2000, 10, &t1Callback);
-Scheduler runner;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void t1Callback() 
+TaskManager taskManager;
+void TemperaturMessen(uint32_t deltaTime); 
+FunctionTask taskTemperaturMessen(TemperaturMessen, MsToTaskTime(10000)); // turn on the led in 400ms
+
+void TemperaturMessen(uint32_t deltaTime)
 {
-    Serial.print("t1: ");
-    Serial.println(millis());
-};
+    // temperaturen messen
+
+    // per mqtt weiterreichen
+    client.publish("esp/test", "hello"); //Topic name
+}
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Scheduler TEST");
-  runner.init();
-  
 
-  Serial.println("Initialized scheduler");
-  
-  runner.addTask(t1);
-  Serial.println("added t1");
-  
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqttServer, mqttPort);
+  //client.setCallback(callback);
+
+  while (!client.connected()) 
+  {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) 
+    {
+      Serial.println("connected"); 
+    } 
+    else 
+    {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
+  taskManager.StartTask(&taskTemperaturMessen); // start with turning it on
 
   delay(5000);
-  
-  t1.enable();
-  Serial.println("Enabled t1");
-}
+};
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-   runner.execute();
+   taskManager.Loop();
 }
