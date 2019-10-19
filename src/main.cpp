acb21077e7f97d7be9f3ca67b120014b78ec7408
sschragen <stephanus@schragen-net.de>
2019-10-19 10:04:40 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <Task.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 const char* ssid = "Schragen2.4"; // Enter your WiFi name
 const char* password =  "warpdrive"; // Enter WiFi password
 const char* mqttServer = "prodesk.fritz.box";
-const int   mqttPort = 37181;
-const char* mqttUser = "otfxknod";
-const char* mqttPassword = "nSuUc1dDLygF";
+const int   mqttPort = 1883;
+const char* mqttUser = "TempLogger";
+const char* mqttPassword = "none";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -17,21 +19,36 @@ TaskManager taskManager;
 void TemperaturMessen(uint32_t deltaTime); 
 FunctionTask taskTemperaturMessen(TemperaturMessen, MsToTaskTime(10000)); // turn on the led in 400ms
 
+#define ONE_WIRE_BUS 2
+#define TEMPERATURE_PRECISION 9
+int numberOfDevices;
+// We'll use this variable to store a found device address
+OneWire *oneWire;
+DallasTemperature *sensors;
+DeviceAddress tempDeviceAddress;
+
 void TemperaturMessen(uint32_t deltaTime)
 {
     // temperaturen messen
+    sensors->requestTemperatures(); // Send the command to get temperatures
+
+    sensors->getAddress(tempDeviceAddress, 0);
+    int vorlauf = sensors->getTempC(tempDeviceAddress);
+    sensors->getAddress(tempDeviceAddress, 1);
+    int ruecklauf = sensors->getTempC(tempDeviceAddress); 
 
     // per mqtt weiterreichen
-    client.publish("esp/test", "hello"); //Topic name
+    client.publish("TempLogger/Vorlauf"  , String  (vorlauf).c_str()); 
+    client.publish("TempLogger/Ruecklauf", String(ruecklauf).c_str());
 }
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
+  
   Serial.print("Connecting");
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -50,7 +67,8 @@ void setup()
     Serial.println("Connecting to MQTT...");
     if (client.connect("ESP8266Client", mqttUser, mqttPassword )) 
     {
-      Serial.println("connected"); 
+      Serial.println("connected");
+      client.publish("TempLogger/IP", WiFi.localIP().toString().c_str()); 
     } 
     else 
     {
@@ -59,6 +77,39 @@ void setup()
       delay(2000);
     }
   }
+
+  oneWire = new OneWire(ONE_WIRE_BUS);
+  sensors = new DallasTemperature(oneWire);
+
+  numberOfDevices = sensors->getDeviceCount();
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");    
+
+  for(int i=0;i<numberOfDevices; i++)
+  {
+      // Search the wire for address
+      if(sensors->getAddress(tempDeviceAddress, i))
+      {
+          Serial.print("Found device ");
+          Serial.print(i, DEC);
+          Serial.print(" with address: ");
+          for (uint8_t i = 0; i < 8; i++)
+          {
+            if (tempDeviceAddress[i] < 16) 
+              Serial.print("0");
+            Serial.print(tempDeviceAddress[i], HEX);
+          }
+          Serial.println();
+      } 
+      else 
+      {
+          Serial.print("Found ghost device at ");
+          Serial.print(i, DEC);
+          Serial.print(" but could not detect address. Check power and cabling");
+      }
+    }
 
   taskManager.StartTask(&taskTemperaturMessen); // start with turning it on
 
